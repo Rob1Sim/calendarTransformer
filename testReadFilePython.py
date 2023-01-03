2b7b95040d4b5cdf4b149c217b51ec8cc8bd7f23
RobinS <robin.le.dev@gmail.com>
2023-01-03 19:21:38 +0100

    Ajout du renomage des lignes

--- testReadFilePython.py
+++ testReadFilePython.py
@@ -1,6 +1,13 @@
 from datetime import datetime;
 
+"""
+    Récupère le calendrier enregistrer 
+    
+"""
 def load_calendar():
+    
+    
+    
     #Ouverture du premier calendrier récupérer sur le web
     try:
         with open('var/calendar/DU2XWMWQ163866.ics', 'r+') as calendar:
@@ -8,18 +15,38 @@ def load_calendar():
     except FileNotFoundError:
         print("Le calendrier est introuvable")
     
-    actualDate= datetime.now().strftime('%d-%m-%Y-%H-%M-%S')
-    
-    try:
-        
-        with open('var/calendar/calendar'+actualDate+'.ics', 'w') as newCalendar:
-            for line in firstCalendar:
-                if line == "SUMMARY:MR" or line == "SUMMARY:MS":
-                    print(line)
-                
-    except FileNotFoundError:
-        print("Echec lors de l'ouverture")
+    return firstCalendar;
     
 
+"""
+    Ecris un nouveau calendrier en formatant l'ancien
+    """
+def write_calendar():
+    
+    firstCalendar = load_calendar();
+    actualDate = datetime.now().strftime('%d-%m-%Y-%H-%M-%S')
+    newFileName = 'calendar'+actualDate+'.ics'
+    try:
+
+        with open('var/calendar/'+newFileName, 'w') as newCalendar:
+            for line in firstCalendar:
+                line = line.replace("\n", "")
+                if "SUMMARY:MR" in line or "SUMMARY:MS" in line:
+                    ligne = ""
+                    #Quitte la boucle lorsque on arrive a la fin du nom de la matière
+                    for lettre in range(len(line)):
+
+                        if line[lettre] == ";":
+                            break
+                        if lettre not in [i for i in range(8,14)]:
+                            ligne += line[lettre]
+                    print(ligne)
+
+
+    except FileNotFoundError:
+        print("Echec lors de l'ouverture")
+
+
+
 if __name__ == '__main__':
-    load_calendar()
+    write_calendar()
