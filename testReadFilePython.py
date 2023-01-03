5f034bb80c80a80c9ff745ae5c5ce57527b4c97e
RobinS <robin.le.dev@gmail.com>
2023-01-03 18:58:39 +0100

    Ajout du début de l'écriture du nouveau fichier

--- testReadFilePython.py
+++ testReadFilePython.py
@@ -1,3 +1,5 @@
+from datetime import datetime;
+
 def load_calendar():
     #Ouverture du premier calendrier récupérer sur le web
     try:
@@ -5,6 +7,18 @@ def load_calendar():
             firstCalendar = calendar.readlines()
     except FileNotFoundError:
         print("Le calendrier est introuvable")
+    
+    actualDate= datetime.now().strftime('%d-%m-%Y-%H-%M-%S')
+    
+    try:
+        
+        with open('var/calendar/calendar'+actualDate+'.ics', 'w') as newCalendar:
+            for line in firstCalendar:
+                if line == "SUMMARY:MR" or line == "SUMMARY:MS":
+                    print(line)
+                
+    except FileNotFoundError:
+        print("Echec lors de l'ouverture")
     
 
 if __name__ == '__main__':
