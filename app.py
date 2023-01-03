0fdaec70d6431173c858666d251ee428642c76e7
RobinS <robin.le.dev@gmail.com>
2023-01-03 20:51:42 +0100

    Ajout de la récupération de l'agenda de l'URCA

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify, send_file
 from os import listdir
+import requests
 import readAndWriteCalender
 
 app = Flask(__name__)
@@ -14,11 +15,18 @@ def return_calendar():
     except Exception as e:
         return str(e)
 
-
+    """
+    Récupère un nouveau calendrier générer
+    """
 def load_calendar(basePath):
     readAndWriteCalender.main()
-    
-    
-    
     return listdir(basePath)[0]
     
+
+    """
+    Récupère la dernière version du calendrier de l'URCA
+    """
+def fetch_calendar():
+    url = "https://caldav.univ-reims.fr/URCA/cache/DU2XWMWQ163866.ics"
+    calendar = requests.get(url,allow_redirects=True)
+    open('var/calendar/DU2XWMWQ163866.ics','wb').write(calendar.content)
